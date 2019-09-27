df0d77e76b8ab361fc7768f068de66c453914dc9
Debra Hodges <dhodges351@gmail.com>
2019-09-26 22:31:02 -0500

    fix path again

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.use(cors());
 app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
-app.use(express.static('dist'));
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -314,8 +313,9 @@ app.delete("/api/contacts/:id", function(req, res) {
   });
 });
 
-app.use('*', (req, res, next) => {
-  res.sendFile('dist/index.html');
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 module.exports = app;
