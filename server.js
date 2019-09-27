59562b7925b3051ffeb963078b4262e243bdde45
Debra Hodges <dhodges351@gmail.com>
2019-09-26 20:59:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ app.use(cors());
 app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.get('*', (_req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
+app.use(express.static('dist'));
+app.get('/', function(req, res) {
+   res.sendFile('index.html');
+});
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
