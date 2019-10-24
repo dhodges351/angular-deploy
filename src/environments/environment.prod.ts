9f4a27e645b099851dbb4ba44333eec7155f3a57
Debra Hodges <dhodges351@gmail.com>
2019-10-23 21:48:13 -0500

    Environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://mighty-forest-64127.herokuapp.com/api',
-  accessKey: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  bucketName: process.env.S3_BUCKET_NAME,
-  MONGODB_URI: process.env.MONGODB_URI
+  accessKey: process.env.RAZZLE_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.RAZZLE_AWS_SECRET_ACCESS_KEY,
+  bucketName: process.env.RAZZLE_S3_BUCKET_NAME
 };
